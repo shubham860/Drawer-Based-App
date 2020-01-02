3d83a4eb13b613194733b4ee22542c420d66ffe6
shubham860 <shubham.chauhan860@gmail.com>
2020-01-02 17:05:55 +0530

    commit-1

---
+++ Drawer/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+/**
+ * - AppSwitchNavigator
+ *    - WelcomeScreen
+ *      - Login Button
+ *      - Sign Up Button
+ *    - AppDrawerNavigator
+ *          - Dashboard - DashboardStackNavigator(needed for header and to change the header based on the                     tab)
+ *            - DashboardTabNavigator
+ *              - Tab 1 - FeedStack
+ *              - Tab 2 - ProfileStack
+ *              - Tab 3 - SettingsStack
+ *            - Any files you don't want to be a part of the Tab Navigator can go here.
+ */
+
+class App extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default App;
